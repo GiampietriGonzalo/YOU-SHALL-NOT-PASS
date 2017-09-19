322b8ce146784f4e318749ccadb42f2bf2d9ebd4
pipita7 <Giampietri.Gonzalo@protonmail.com>
2017-09-19 11:17:19 -0300

    main gui

--- codigo/you-shall-not-pass/src/GUI/GUI.java
+++ codigo/you-shall-not-pass/src/GUI/GUI.java
@@ -31,16 +31,10 @@ public class GUI {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI window = new GUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		try {
+			GUI window = new GUI();
+			window.frame.setVisible(true);
+		} catch (Exception e) {e.printStackTrace();}
 	}
 
 	/**
